86ec063f02489f0ffb6405494600984e8b31d0d0
Brian Yu <brian@cs.harvard.edu>
2020-08-11 22:14:46 -0400

    Add questions

--- questions.js
+++ questions.js
@@ -27,6 +27,22 @@ const QUESTIONS = [
     "What do you believe to be some of the most pressing health issues today? Why?",
     "What do you feel are the negative or restrictive aspects of medicine from a professional standpoint?",
     "If you had to choose between clinical and academic medicine as a profession, which would you pick? What do you feel you might lose by being forced to choose?",
-    "What do you feel are the social responsibilities of a physician?"
+    "What do you feel are the social responsibilities of a physician?",
+    "What do you consider an important/the most important social problem facing the United States today and why?",
+    "How do you think national health insurance affects physicians, patients, and society?",
+    "In what manner and to what degree do you stay in touch with current events?",
+    "What books, films, or other media come to mind as having been particularly important to your sciences/non-sciences education?",
+    "Are you aware of any current controversies in the area of medical ethics? List and discuss some of these.",
+    "Have you personally encountered any moral dilemmas to date? Of what nature?",
+    "What different feelings and issues might you experience with a terminally ill patient, as opposed to other patients?",
+    "If you are a woman, how has your gender impacted your decision to pursue a medical career?",
+    "What special qualities do you feel you possess that set you apart from other medical school candidates? What makes you unique or different as a medical school candidate?",
+    "What kind of medical schools are you applying to, and why?",
+    "Pick any specific medical school to which you are applying, and tell the interviewer about it. What makes this school particularly desirable to you?",
+    "What general and specific skills would you hope an ideal medical school experience would give you? How might your ideal school achieve that result?",
+    "Discuss your decision to pursue medicine. When did you decide to become an MD, and why?",
+    "Why did you decide to choose medicine and not some other field where you can help others, such as nursing, physical therapy, pharmacology, psychology, education, or social work?",
+    "How have you tested your motivation to become an MD? Please explain.",
+    "What will you do if you are not accepted to medical school this year? Have you an alternative career plan?"
 ];
 
